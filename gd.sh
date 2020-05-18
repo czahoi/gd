eb677a21db954f17effdbcc11096e09a018f2059
czahoi <37472306+czahoi@users.noreply.github.com>
2020-05-18 11:13:09 +0800

    Update gd.sh

--- gd.sh
+++ gd.sh
@@ -8,6 +8,6 @@ link=${link#*d/}
 link=${link%?usp*}
 echo "输入目录名"
 read -p "请输入:" dir
-dir=${dir};
+dir=/${dir};
 echo $dir
-gclone copy  gc:{$link}  gc:{0AIH2GMtahzrcUk9PVA}'\'$dir  --ignore-existing --tpslimit 3 --transfers 3 --drive-chunk-size 32M --disable ListR --drive-acknowledge-abuse  --create-empty-src-dirs -v --progress --drive-server-side-across-configs
+echo gclone copy  gc:{$link}  gc:{0AIH2GMtahzrcUk9PVA}$dir  --ignore-existing --tpslimit 3 --transfers 3 --drive-chunk-size 32M --disable ListR --drive-acknowledge-abuse  --create-empty-src-dirs -v --progress --drive-server-side-across-configs
